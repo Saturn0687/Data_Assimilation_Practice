20cfb3e9b234754b26b5b6f93f3242159a2be052
Chi-Jui Chen <chjchen@asiaa.sinica.edu.tw>
2020-01-14 02:04:39 +0800

    Perturbed lorentz96 model

--- lorenz96_differentF.py
+++ lorenz96_differentF.py
@@ -4,7 +4,5 @@ Definition of the Lorenz 40-variable (Lorenz 96) model
 import numpy as np
 
 def f(t, y, F):
-    #np.random.seed(21010+np.array([t]).astype(int))
-    #error_of_F = 1
-    #F = F + error_of_F * np.random.randn(y.size) # test
+    #
     return (np.roll(y, -1) - np.roll(y, 2)) * np.roll(y, 1) - y + F
